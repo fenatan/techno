00e10de97ac7cdb252ac47e676a2504c50711085
Felipe Natan <felipenatan@novaandradina.org>
2020-06-08 21:49:45 -0400

    add: filters vue

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const vm = new Vue({
   data: {
     produtos: []
   },
-
+  filters:{
+    numeroPreco(valor){
+        return valor.toLocaleString("pt-BR", {style:"currency",
+         currency: "BRL"})
+    }
+  },
   methods: {
     fetchProdutos() {
       fetch("./api/produtos.json")
